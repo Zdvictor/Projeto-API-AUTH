a2f384129b4a4915b6efbf1bba75abef82178d1b
Victor <victor.zaidir@gmail.com>
2024-07-10 18:57:15 -0300

    Deploy

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : 'localhost',
-      user : 'root',
-      password : '1234',
-      database : 'apiusers'
+      host : process.env.DB_HOST,
+      user : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DBNAME
     }
   });
 
